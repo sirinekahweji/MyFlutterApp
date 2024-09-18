1728985ecb6750aba6ba3ed916544de584969089
sirine kahweji <sirinekahweji5@gmail.com>
2024-09-18 17:37:07 +0200

    stateless ninja id card

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,104 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Home()));
+  runApp(MaterialApp(home: NinjaCard()));
 }
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class NinjaCard extends StatelessWidget {
+  const NinjaCard({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.purple[50],
       appBar: AppBar(
-        title: const Text('my first app'),
+        title: Text(
+          'Ninja ID Card',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
         centerTitle: true,
-        backgroundColor: const Color.fromARGB(255, 244, 184, 215),
+        backgroundColor: const Color.fromARGB(255, 159, 6, 126),
+        elevation: 0.0,
       ),
-      body: Row(
-        children: [
-          Expanded(
-            child: Image.asset('assets/space.jpg'),
-            flex: 3,
-          ),
-          Expanded(
-            flex: 3,
-            child: Container(
-              padding: EdgeInsets.all(20.0),
-              color: Colors.pink[200],
-              child: Text('con1'),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('syrineninja.png'),
+                radius: 60.0,
+              ),
             ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              padding: EdgeInsets.all(20.0),
-              color: Colors.pink[300],
-              child: Text('con2'),
+            Divider(
+              height: 90.0,
+              color: const Color.fromARGB(255, 168, 10, 134),
             ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(20.0),
-              color: Colors.pink[400],
-              child: Text('con3'),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey[500],
+                letterSpacing: 2.0,
+              ),
             ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add), // Using an icon is more common for FABs.
-        backgroundColor: const Color.fromARGB(255, 244, 184, 215),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'Syrina ',
+              style: TextStyle(
+                color: const Color.fromARGB(255, 168, 10, 134),
+                letterSpacing: 2.0,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              'CURRENT NINJA LEVEL',
+              style: TextStyle(
+                color: Colors.grey[500],
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              '6 ',
+              style: TextStyle(
+                color: const Color.fromARGB(255, 168, 10, 134),
+                letterSpacing: 2.0,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: [
+                Icon(Icons.email, color: Color.fromARGB(255, 168, 10, 134)),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  'Syrina@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
